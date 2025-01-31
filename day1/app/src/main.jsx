50b3494a3d41f646e6b1701f382aaf839ae8ebd2
koduru-sardhika <kodurusardhika@gmail.com>
2025-01-31 22:09:27 +0530

    msg

--- day1/app/src/main.jsx
+++ day1/app/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+//import App from './App.jsx'
+import Cbc from './component/Cbc.jsx'
+import Fbc from './component/Fbc.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Cbc/>
+    <Fbc/>
+  </StrictMode>
 )
